592a2c850119d6c547a1639437c4f409fb0c9381
garageinc <9-b-rinat@rambler.ru>
2023-12-27 12:08:24 +0300

    :sparkles: (feat): Transaction status popup

--- src/components/Popups/TransactionPopup.tsx
+++ src/components/Popups/TransactionPopup.tsx
@@ -1,3 +1,4 @@
+import { GreyCard } from 'components/Card'
 import { useContext } from 'react'
 import { AlertCircle, CheckCircle } from 'react-feather'
 import styled, { ThemeContext } from 'styled-components'
@@ -28,18 +29,21 @@ export default function TransactionPopup({
   const theme = useContext(ThemeContext)
 
   return (
-    <RowNoFlex>
-      <div style={{ paddingRight: 16 }}>
-        {success ? <CheckCircle color={theme.green} size={24} /> : <AlertCircle color={theme.red} size={24} />}
-      </div>
-      <AutoColumn gap="8px">
-        <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hash.slice(0, 8) + '...' + hash.slice(58, 65)}</TYPE.body>
-        {chainId && hash && (
-          <ExternalLink href={getExplorerLink(chainId, hash, ExplorerDataType.TRANSACTION)}>
-            View on Explorer
-          </ExternalLink>
-        )}
-      </AutoColumn>
-    </RowNoFlex>
+    <GreyCard>
+      <RowNoFlex>
+        <div style={{ paddingRight: 16 }}>
+          {success ? <CheckCircle color={theme.green} size={24} /> : <AlertCircle color={theme.red} size={24} />}
+        </div>
+        <AutoColumn gap="8px">
+          <TYPE.body fontWeight={500}>{summary ?? 'Hash: ' + hash.slice(0, 8) + '...' + hash.slice(58, 65)}</TYPE.body>
+
+          {chainId && hash && (
+            <ExternalLink href={getExplorerLink(chainId, hash, ExplorerDataType.TRANSACTION)}>
+              View on Explorer
+            </ExternalLink>
+          )}
+        </AutoColumn>
+      </RowNoFlex>
+    </GreyCard>
   )
 }
