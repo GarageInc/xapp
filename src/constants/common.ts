f7ef071f90f485660503cf71ca846bfb0e475283
denis_Kuchko <useradmin@MacBook-Air-User-3.local>
2023-12-28 14:00:04 +0100

    rewrite formattedDuration function

---
+++ src/constants/common.ts
@@ -0,0 +1,9 @@
+export const CONVERTED_DURATION_UNITS = {
+  years: 'y',
+  months: 'm',
+  weeks: 'w',
+  days: 'd',
+  hours: 'h',
+  minutes: 'min',
+  seconds: 's',
+} as const
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,6 @@
+import { CONVERTED_DURATION_UNITS } from 'constants/common'
 import { Duration, format } from 'date-fns'
-import { isObjectEmpty } from 'utils/common'
+import { DurationUnit } from 'date-fns/types'
 
 function isValidDate(date: unknown) {
   return date instanceof Date && !isNaN(date as unknown as number)
@@ -13,18 +14,35 @@ export const fullDate = (date: number, formatString = `dd.LL.yyyy HH:mm:ss`) => 
   return format(jsDate, formatString)
 }
 
+/**
+ * @description convert Duration object in simple string
+ * @example
+ * formattedDuration({months: 1,days: 1}); // '1m 1d'
+ * formattedDuration({months: 1,days: 1, seconds: 1}); // '1m 1d'
+ * formattedDuration({hours: 1}); // '1h 1m'
+ * formattedDuration({}); // '0s'
+ */
 export const formattedDuration = (duration: Duration) => {
-  if (isObjectEmpty(duration)) return '0s'
+  const keys = Object.keys(duration) as DurationUnit[]
 
-  if (duration.months) {
-    return `${duration.months}m ${duration.days}d`
-  } else if (duration.days) {
-    return `${duration.days}d ${duration.hours}h`
-  } else if (duration.hours) {
-    return `${duration.hours}h ${duration.minutes}min`
-  } else if (duration.minutes) {
-    return `${duration.minutes}min ${duration.seconds}s`
+  // check is duration empty or duration has  only seconds
+  if (keys.length === 0) return '0s'
+
+  if (keys.length === 1 && 'seconds' in duration) return `${duration.seconds}s`
+
+  const measurementUnitKeys = Object.keys(CONVERTED_DURATION_UNITS) as DurationUnit[]
+
+  const firstKey = keys[0]
+  const secondKey = keys[1]
+
+  const firstPart = `${duration[firstKey]}${CONVERTED_DURATION_UNITS[firstKey]}`
+
+  if (secondKey) {
+    return `${firstPart} ${duration[secondKey]}${CONVERTED_DURATION_UNITS[secondKey]}`
   } else {
-    return `${duration.seconds}s`
+    const firstKeyIndex = measurementUnitKeys.findIndex((value) => value === firstKey)
+    const secondKey = measurementUnitKeys[firstKeyIndex + 1] ?? measurementUnitKeys[measurementUnitKeys.length - 1]
+
+    return `${firstPart} 0${CONVERTED_DURATION_UNITS[secondKey]}`
   }
 }
